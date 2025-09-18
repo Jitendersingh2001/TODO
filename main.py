7f1f57dcdf9694dd8b0a41094cc17ebf5aed331a
Jitender Singh <jitendersingh3549@gmail.com>
2025-09-18 10:22:47 +0530

    chore: add a blank line for improved code readability in main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI(title="Todo App")
 # Include all API endpoints from app.api
 app.include_router(api_router)
 
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Todo App!"}
